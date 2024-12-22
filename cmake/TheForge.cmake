1e0637533304e89fa399e8407580ecec7405595c
Bruno Henrique Glowaski Morais <bruno.glowaski@outlook.com>
2024-12-21 22:32:09 -0400

    misc: add AssetPipeline executable

--- cmake/TheForge.cmake
+++ cmake/TheForge.cmake
@@ -326,3 +326,38 @@ endif()
 tf_add_shader(Renderer ${TFORGE_COMMON_DIR}Application/Fonts/Shaders/FSL/Fonts_ShaderList.fsl)
 tf_add_shader(Renderer ${TFORGE_COMMON_DIR}Application/UI/Shaders/FSL/UI_ShaderList.fsl)
 tf_add_shader(Renderer ${TFORGE_COMMON_DIR}Resources/AnimationSystem/Animation/Shaders/FSL/Animation_ShaderList.fsl)
+
+# Asset Pipeline
+set(ITOOLFILESYSTEM_SRC 
+  ${TFORGE_COMMON_DIR}Utilities/FileSystem/ToolFileSystem.c
+  ${TFORGE_COMMON_DIR}OS/Linux/LinuxToolsFileSystem.c
+)
+file(GLOB ASSETPIPELINEBASE_SRC ${TFORGE_COMMON_DIR}Tools/AssetPipeline/src/*.cpp)
+set(BUNYARCHIVE_SRC 
+  ${TFORGE_COMMON_DIR}Tools/BunyArchive/Buny.c
+)
+file(GLOB LZ4_SRC ${TFORGE_COMMON_DIR}Utilities/ThirdParty/OpenSource/lz4/*.c)
+file(GLOB MESHOPTIOMIZER_SRC ${TFORGE_COMMON_DIR}Tools/ThirdParty/OpenSource/meshoptimizer/src/*.cpp)
+file(GLOB MESHOPTIOMIZER_SRC ${TFORGE_COMMON_DIR}Tools/ThirdParty/OpenSource/meshoptimizer/src/*.cpp)
+set(TRESSFX_SRC ${TFORGE_COMMON_DIR}Resources/AnimationSystem/ThirdParty/OpenSource/TressFX/TressFXAsset.cpp)
+file(GLOB ZSTD_COMMON_SRC ${TFORGE_COMMON_DIR}Utilities/ThirdParty/OpenSource/zstd/common/*.c)
+file(GLOB ZSTD_COMPRESS_SRC ${TFORGE_COMMON_DIR}Utilities/ThirdParty/OpenSource/zstd/compress/*.c)
+file(GLOB ISPCTEXTURECOMPRESSOR_OBJ ${TFORGE_COMMON_DIR}Tools/ThirdParty/OpenSource/ISPCTextureCompressor/ispc_texcomp/*.o)
+
+set(ASSETPIPELINE_SRC 
+  ${ITOOLFILESYSTEM_SRC} 
+  ${ASSETPIPELINEBASE_SRC} 
+  ${BUNYARCHIVE_SRC}
+  ${LZ4_SRC}
+  ${MESHOPTIOMIZER_SRC}
+  ${TRESSFX_SRC}
+  ${ZSTD_COMMON_SRC}
+  ${ZSTD_COMPRESS_SRC}
+)
+
+add_executable(AssetPipeline ${ASSETPIPELINE_SRC})
+target_link_libraries(AssetPipeline 
+  PRIVATE OS ozz_base ozz_animation ozz_animation_offline
+  ${ISPCTEXTURECOMPRESSOR_OBJ}
+)
+
