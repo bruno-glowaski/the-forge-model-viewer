5c5ce65af6403b08c2b4fbaf35231240d64b19b2
Bruno Henrique Glowaski Morais <bruno.glowaski@outlook.com>
2024-12-23 22:18:03 -0400

    refactor: split AssetPipeline file

---
+++ cmake/TheForgeAssetPipeline.cmake
@@ -0,0 +1,33 @@
+set(ITOOLFILESYSTEM_SRC ${TFORGE_COMMON_DIR}Utilities/FileSystem/ToolFileSystem.c)
+file(GLOB ASSETPIPELINEBASE_SRC ${TFORGE_COMMON_DIR}Tools/AssetPipeline/src/*.cpp)
+set(BUNYARCHIVE_SRC 
+  ${TFORGE_COMMON_DIR}Tools/BunyArchive/Buny.c
+)
+file(GLOB LZ4_SRC ${TFORGE_COMMON_DIR}Utilities/ThirdParty/OpenSource/lz4/*.c)
+file(GLOB MESHOPTIOMIZER_SRC ${TFORGE_COMMON_DIR}Tools/ThirdParty/OpenSource/meshoptimizer/src/*.cpp)
+file(GLOB MESHOPTIOMIZER_SRC ${TFORGE_COMMON_DIR}Tools/ThirdParty/OpenSource/meshoptimizer/src/*.cpp)
+set(TRESSFX_SRC ${TFORGE_COMMON_DIR}Resources/AnimationSystem/ThirdParty/OpenSource/TressFX/TressFXAsset.cpp)
+file(GLOB ZSTD_COMMON_SRC ${TFORGE_COMMON_DIR}Utilities/ThirdParty/OpenSource/zstd/common/*.c)
+file(GLOB ZSTD_COMPRESS_SRC ${TFORGE_COMMON_DIR}Utilities/ThirdParty/OpenSource/zstd/compress/*.c)
+file(GLOB ISPCTEXTURECOMPRESSOR_OBJ ${TFORGE_COMMON_DIR}Tools/ThirdParty/OpenSource/ISPCTextureCompressor/ispc_texcomp/*.o)
+
+if (LINUX)
+  list(APPEND ITOOLFILESYSTEM_SRC ${TFORGE_COMMON_DIR}OS/Linux/LinuxToolsFileSystem.c)
+endif()
+
+set(ASSETPIPELINE_SRC 
+  ${ITOOLFILESYSTEM_SRC} 
+  ${ASSETPIPELINEBASE_SRC} 
+  ${BUNYARCHIVE_SRC}
+  ${LZ4_SRC}
+  ${MESHOPTIOMIZER_SRC}
+  ${TRESSFX_SRC}
+  ${ZSTD_COMMON_SRC}
+  ${ZSTD_COMPRESS_SRC}
+)
+
+add_executable(AssetPipeline ${ASSETPIPELINE_SRC})
+target_link_libraries(AssetPipeline 
+  PRIVATE OS ozz_base ozz_animation ozz_animation_offline
+  ${ISPCTEXTURECOMPRESSOR_OBJ}
+)
